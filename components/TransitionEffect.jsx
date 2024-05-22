ef2309dfa5bc90522c864f8acb84c238a1476b08
dpearyer <dpear1050@gmail.com>
2024-05-22 01:13:17 -0400

    added mobile navbar

--- components/TransitionEffect.jsx
+++ components/TransitionEffect.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { AnimatePresence } from "framer-motion"
+import { AnimatePresence, delay, motion } from "framer-motion"
 import { usePathname } from "next/navigation";
 import Effect from "./Effect";
 
@@ -13,7 +13,14 @@ const TransitionEffect = () => {
         <div key={pathname}>
             <div className="h-screen w-screen fixed top-0 left-0 right-0 
             pointer-events-none z-40 flex">
-                <Effect /></div>
+                <Effect />
+                </div>
+                <motion.div className="h-screen w-screen fixed bg-primary top-0 pointer-events-none" 
+                initial={{opacity: 1}}
+                animate={{
+                    opacity:0,
+                    transition: {delay:1, duration:0.4, ease: "easeInOut"},
+                    }} />
         </div>
     </AnimatePresence>
   </>
