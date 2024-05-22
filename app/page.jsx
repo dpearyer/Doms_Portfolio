c8c90c41de188f74e8c5a64fcfe448714615fd2b
dpearyer <dpear1050@gmail.com>
2024-05-22 01:48:00 -0400

    download resume button

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,38 @@
+import { Buttonimrt } from "@/components/ui/button";
+import { FiDownload } from "react-icons/fi";
+import { Button } from "@/components/ui/button";
 
-export default function Home() {
-  return <main>homepage</main>;
-
+const Home = () => {
+  return (
+    <section className="h-full">
+      <div className="container mx-auto h-full" >
+        <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
+          {/*text */}
+          <div className="text-center xl:text-left">
+            <span className="text-xl">Software Engineer</span>
+            <h1 className="h1">
+              Hello I'm <br /> <span className="text-accent">Dominique Pearyer</span>
+            </h1>
+            <p className="max-w-[500px] mb-9 text-white/80">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+              Perferendis quae iusto amet obcaecati voluptas eius magnam 
+              </p>
+              {/*button and socials */}
+              <div className="flex flex-col xl:flex-row items-center gap-8">
+                <Button variant="outline" size="lg" className="uppercase items-center gap-2">
+                  <span>Download Resume</span>
+                  <FiDownload className="text-xl" />
+                </Button>
+                <div>socials</div>
+              </div>
+          </div>
+          {/*text */}
+          <div>photo</div>
+        </div>
+      </div>
+    </section>
+  )
 }
+
+export default Home
+
