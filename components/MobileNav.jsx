b97484fa3250b0359ba27307d10b9413776fa309
dpearyer <dpear1050@gmail.com>
2024-05-22 01:26:52 -0400

    nav, mobile nav, header v1 completed

--- components/MobileNav.jsx
+++ components/MobileNav.jsx
@@ -45,7 +45,7 @@ const MobileNav = () => {
         <nav className="flex flex-col justify-center items-center gap-8">
             {links.map((link, index)=>{
                 return(
-                    <Link href={link.path} key={index} className={`text-xl capitalize hover:text-accent transition-all`}>
+                    <Link href={link.path} key={index} className={`${link.path === pathname && "text-accent border-b-2 border-accent"} text-xl capitalize hover:text-accent transition-all`}>
                     {link.name}
                     </Link>
                 )
