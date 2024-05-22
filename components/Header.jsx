821c5e926d4c122810cb78c15a7065a0d8ca0fd2
dpearyer <dpear1050@gmail.com>
2024-05-21 23:21:22 -0400

    added navbar and contact btn

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,11 +1,27 @@
 import Link from "next/link";
 import { Button } from "./ui/button";
+import Nav from "./Nav";
 
 const Header = () => {
   return (
-    <header className="py-8 xl:py-12 text-white bg-pink-50/20">
-        <div className="container mx-auto">header</div>
+    <header className="py-8 xl:py-12 text-white">
+        <div className="container mx-auto">
+            {/* logo */}
+            <Link href="/">
+            <h1 className="text-4xl font-semibold">
+                Dominique <span className="text-accent">.</span>
+                </h1>
+            </Link>
 
+
+            {/* Desktop Nav & contact me btn */}
+            <div className="hidden xl:flex">
+            <Nav />
+            <Link href="/contact">
+               <Button>Contact Me</Button> 
+            </Link>
+            </div>
+        </div>
     </header>
   )
 }
