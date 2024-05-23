ffae0e38ae579d7e9acd19ce1790e002c7f6b70d
dpearyer <dpear1050@gmail.com>
2024-05-23 18:42:46 -0400

    skill pg v1 finished

--- app/skills/page.jsx
+++ app/skills/page.jsx
@@ -35,13 +35,13 @@ const skills =[
     href:""
   },
   {
-    num: '05',
+    num: '06',
     title:'HTML/CSS/Javascript',
     description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla vel iure voluptate dolorem voluptatum disti',
     href:""
   },
   {
-    num: '05',
+    num: '07',
     title:'Databases',
     description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla vel iure voluptate dolorem voluptatum disti',
     href:""
@@ -61,20 +61,28 @@ const Skills = () => {
         opacity: 1, 
         transition: {delay: 2.4, duration:0.4, ease: "easeIn"},
         }}
+        className="grid grid-cols-1 md:grid-cols-2 gap-[60px]"
         >
           {skills.map((skill, index)=>{
-            return <div key={index}>
+            return <div 
+            key={index}
+            className="flex-1 flex flex-col justify-center gap-6 group"
+            >
               {/*top */}
-              <div>
-              <div>{skill.num}</div>
-              <Link href={skill.href}>
-                <BsArrowDownRight/>
+              <div className="w-full flex justify-between items-center">
+              <div className="text-5xl font-extrabold text-outline text-transparent 
+              group-hover:text-outline-hover transition-all duration-500">{skill.num}</div>
+              <Link href={skill.href} className="w-[70px] h-[70px] rounded-full
+               bg-white group-hover:bg-accent transition-all duration-500 flex
+               justify-center items-center hover:-rotate-90">
+                <BsArrowDownRight className="text-primary text-3xl"/>
               </Link>
             </div>
             {/*title*/}
-            <h2>{skill.title}</h2>
+            <h2 className="text-[42px] font-bold leading-none text-white
+            group-hover:text-accent transition-all duration-500">{skill.title}</h2>
             {/*description */}
-            <p>{skill.description}</p>
+            <p className="text-white/60">{skill.description}</p>
             {/*border */}
             <div className="border-b border-white/20 w-full"></div>
             </div>
